cf5c8daef287c2594dc56fef0131a77e96a15b64
noumi <nomiwen@outlook.de>
2025-05-20 06:12:08 +0200

    Common: Add float types

--- include/common/bit.h
+++ include/common/bit.h
@@ -10,3 +10,6 @@
 u32 common_Clz(const u32 data);
 
 u32 common_Rotl(const u32 data, const int amt);
+
+f32 common_ToF32(const u32 data);
+f64 common_ToF64(const u64 data);
--- src/common/bit.c
+++ src/common/bit.c
@@ -5,6 +5,8 @@
 
 #include "common/bit.h"
 
+#include <string.h>
+
 u32 common_Clz(const u32 data) {
     for (u32 i = 0; i < 32; i++) {
         if ((data & (1 << (31 - i))) != 0) {
@@ -21,4 +23,20 @@ u32 common_Rotl(const u32 data, const int amt) {
     }
 
     return (data << (amt & 31)) | (data >> (32 - (amt & 31)));
+}
+
+f32 common_ToF32(const u32 data) {
+    f32 f;
+
+    memcpy(&f, &data, sizeof(f));
+
+    return f;
+}
+
+f64 common_ToF64(const u64 data) {
+    f64 f;
+
+    memcpy(&f, &data, sizeof(f));
+
+    return f;
 }
