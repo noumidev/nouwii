3309e09de326862a84d6f1cd266a432c543c81a8
noumi <nomiwen@outlook.de>
2025-05-20 06:12:16 +0200

    Common: Add 64-bit bswap

--- include/common/bswap.h
+++ include/common/bswap.h
@@ -10,3 +10,4 @@
 u8 common_Bswap8(const u8 data);
 u16 common_Bswap16(const u16 data);
 u32 common_Bswap32(const u32 data);
+u64 common_Bswap64(const u64 data);
--- src/common/bswap.c
+++ src/common/bswap.c
@@ -16,3 +16,13 @@ u16 common_Bswap16(const u16 data) {
 u32 common_Bswap32(const u32 data) {
     return (data >> 24) | ((data >> 8) & 0xFF00) | ((data & 0xFF00) << 8) | (data << 24);
 }
+
+u64 common_Bswap64(const u64 data) {
+    u64 n = data;
+
+    n = ((n & 0x00000000FFFFFFFFULL) << 32) | ((n & 0xFFFFFFFF00000000ULL) >> 32);
+    n = ((n & 0x0000FFFF0000FFFFULL) << 16) | ((n & 0xFFFF0000FFFF0000ULL) >> 16);
+    n = ((n & 0x00FF00FF00FF00FFULL) <<  8) | ((n & 0xFF00FF00FF00FF00ULL) >>  8);
+
+    return n;
+}
