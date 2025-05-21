16d7fbac13ff13aba3c26ff7476a5ab542583176
noumi <nomiwen@outlook.de>
2025-05-21 07:08:24 +0200

    PI: Add CONSOLE_TYPE

--- src/hw/pi.c
+++ src/hw/pi.c
@@ -10,11 +10,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CONSOLE_TYPE (2 << 28)
+
 #define MAKEFUNC_PI_READIO(size)                                     \
 u##size pi_ReadIo##size(const u32 addr) {                            \
     printf("PI Unimplemented read%d (address: %08X)\n", size, addr); \
     exit(1);                                                         \
 }                                                                    \
+
+enum {
+    PI_CONSOLE_TYPE = 0xC00302C,
+};
 
 void pi_Initialize() {
 
@@ -30,5 +36,17 @@ void pi_Shutdown() {
 
 MAKEFUNC_PI_READIO(8)
 MAKEFUNC_PI_READIO(16)
-MAKEFUNC_PI_READIO(32)
 MAKEFUNC_PI_READIO(64)
+
+u32 pi_ReadIo32(const u32 addr) {
+    switch (addr) {
+        case PI_CONSOLE_TYPE:
+            printf("PI_CONSOLE_TYPE read32\n");
+
+            return CONSOLE_TYPE;
+        default:
+            printf("PI Unimplemented read32 (address: %08X)\n", addr);
+
+            exit(1);
+    }
+}
