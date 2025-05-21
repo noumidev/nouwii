356b4b8952319a7a17a7c35d7dc4d227a512dd54
noumi <nomiwen@outlook.de>
2025-05-21 14:21:27 +0200

    PI: Add I/O writes, INTMASK

--- include/hw/pi.h
+++ include/hw/pi.h
@@ -10,6 +10,9 @@
 #define MAKEDECL_PI_READIO(size)         \
 u##size pi_ReadIo##size(const u32 addr); \
 
+#define MAKEDECL_PI_WRITEIO(size)                          \
+void pi_WriteIo##size(const u32 addr, const u##size data); \
+
 void pi_Initialize();
 void pi_Reset();
 void pi_Shutdown();
@@ -18,3 +21,8 @@ MAKEDECL_PI_READIO(8)
 MAKEDECL_PI_READIO(16)
 MAKEDECL_PI_READIO(32)
 MAKEDECL_PI_READIO(64)
+
+MAKEDECL_PI_WRITEIO(8)
+MAKEDECL_PI_WRITEIO(16)
+MAKEDECL_PI_WRITEIO(32)
+MAKEDECL_PI_WRITEIO(64)
--- src/hw/pi.c
+++ src/hw/pi.c
@@ -18,16 +18,29 @@ u##size pi_ReadIo##size(const u32 addr) {                            \
     exit(1);                                                         \
 }                                                                    \
 
+#define MAKEFUNC_PI_WRITEIO(size)                                                       \
+void pi_WriteIo##size(const u32 addr, const u##size data) {                             \
+    printf("PI Unimplemented write%d (address: %08X, data: %02X)\n", size, addr, data); \
+    exit(1);                                                                            \
+}                                                                                       \
+
 enum {
+    PI_INTMASK      = 0xC003004,
     PI_CONSOLE_TYPE = 0xC00302C,
 };
+
+typedef struct Context {
+    u32 intmask;
+} Context;
+
+static Context ctx;
 
 void pi_Initialize() {
 
 }
 
 void pi_Reset() {
-
+    memset(&ctx, 0, sizeof(ctx));
 }
 
 void pi_Shutdown() {
@@ -48,5 +61,23 @@ u32 pi_ReadIo32(const u32 addr) {
             printf("PI Unimplemented read32 (address: %08X)\n", addr);
 
             exit(1);
+    }
+}
+
+MAKEFUNC_PI_WRITEIO(8)
+MAKEFUNC_PI_WRITEIO(16)
+MAKEFUNC_PI_WRITEIO(64)
+
+void pi_WriteIo32(const u32 addr, const u32 data) {
+    switch (addr) {
+        case PI_INTMASK:
+            printf("PI_INTMASK write32 (data: %08X)\n", data);
+
+            ctx.intmask = data;
+            break;
+        default:
+            printf("PI Unimplemented write32 (address: %08X, data: %08X)\n", addr, data);
+
+            exit(1); 
     }
 }
